4dd849446620f5a6e139ff4863a2f3ccf2657222
jvratanar@email.com <jvratanar@email.com>
2021-03-17 19:35:56 +0100

    221. Spring PetTypeFormatter added.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.formatters;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Set;
+
+@Component
+/*
+When Spring parses PetType it will use this formatter.
+*/
+public class PetTypeFormatter implements Formatter<PetType> {
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Set<PetType> findPetTypes = this.petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+
+    @Override
+    public String print(PetType object, Locale locale) {
+        return object.getName();
+    }
+}
